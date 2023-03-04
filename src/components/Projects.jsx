1f05c0a03d3782da2f15ddc1f108b2f00ccd3334
Strahinja <vujinovicstrahinja123@gmail.com>
2023-03-04 22:07:56 +0100

    Refactor code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,12 +8,13 @@ import { ProjectItem } from './ProjectItem'
 
 export const Projects = () => {
   return (
-    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 '>
       <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
 
       <p className='text-center py-8 font-bold'>My portfolio features a variety of projects that showcase my skills and expertise in web development. Each project is built using modern web technologies  including HTML, CSS, JavaScript frameworks such as React and Nextjs.</p>
-      <div className='grid sm:grid-cols-2 gap-10'>
-        <ProjectItem img={FirstImg} title='Tax Calculator'  desc='I created a tax calculator using React, which allows users to easily calculate their tax for the year.' />
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 '>
+        <ProjectItem img={FirstImg} title='Tax Calculator'   desc='I created a tax calculator using React, which allows users to easily calculate their tax for the year.' link='https://6403ab33c15eee780f63d50d--aquamarine-rabanadas-8c832e.netlify.app/'></ProjectItem> 
+        
         <ProjectItem img={SecondImg} title='XBOX' desc='I built copy of a website using React and Tailwind that draws inspiration from Xbox'/>
         <ProjectItem img={ThirdImg} title='Metaverse' desc='Website created using Framer Motion.The website features UI and UX interactive animations.' />
         < ProjectItem img={FourthImg} title='Gost Shop' desc='Project that Im particularly proud of is a  website that I built from scratch using React and Redux.'  /> 
